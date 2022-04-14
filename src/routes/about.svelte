bf74517bc4052feb72697f4dfc6ad1452917fef1
skpayaya <sashi.kumar.patelskp@gmail.com>
2022-04-14 10:24:49 +0530

    pokedex-app

---
+++ src/routes/about.svelte
@@ -0,0 +1,3 @@
+<svelte:head><title>About the SvelteKit PokeDex</title></svelte:head>
+<h1>About</h1>
+<p>Uses Pokemon API to build pokedex</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { pokemonList } from '../stores/pokestore';
+	// $: console.log($pokemonList);
+</script>
+
+<svelte:head><title>SvelteKit PokeDex</title></svelte:head>
+<h1>SvelteKit PokeDex</h1>
+
+{#each $pokemonList as pokemon}
+	<p>{pokemon.name}</p>
+{/each}
