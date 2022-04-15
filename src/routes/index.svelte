3d9d078312ebb02b643f6f22cb79b77d16648b7b
skpayaya <sashi.kumar.patelskp@gmail.com>
2022-04-15 23:51:50 +0530

    filter logic added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,33 @@
 	import { pokemonList } from '../stores/pokestore';
 	import PokemonCard from '../components/pokemonCard.svelte';
 	// $: console.log($pokemonList);
+	let searchTerm = '';
+	let filteredPokemon = [];
+
+	$: {
+		if (searchTerm) {
+			// search the pokemon
+			filteredPokemon = $pokemonList.filter(
+				(pokemon) =>
+					pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+					pokemon.id.toString().includes(searchTerm)
+			);
+		} else {
+			filteredPokemon = [...$pokemonList];
+		}
+	}
 </script>
 
 <svelte:head><title>SvelteKit PokeDex</title></svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">SvelteKit PokeDex</h1>
+<input
+	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	type="text"
+	bind:value={searchTerm}
+	placeholder="Search Pokemon"
+/>
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1 lg:grid-cols-3">
-	{#each $pokemonList as pokemon}
+	{#each filteredPokemon as pokemon}
 		<PokemonCard {pokemon} />
 	{/each}
 </div>
